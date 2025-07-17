bb57dd7565107cee108eb630bdb6dde3daf849a0
sasikanthreddykummitha@gmail.com <sasikanthreddykummitha@gmail.com>
2025-07-17 11:39:50 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "terrarg" {
   name     = "terrarg"
   location = "East US"
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = "terrastorageacc02"  # must be globally unique and lowercase
+  resource_group_name      = "terra-rg"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
